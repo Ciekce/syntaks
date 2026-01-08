41335cc569a725804a05317c7ab777a5b3bedd21
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-01-08 08:06:55 +0000

    yea

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-use crate::board::Position;
-use crate::core::Player;
-
 mod bitboard;
 mod board;
 mod core;
 mod movegen;
 mod perft;
+mod road;
 mod takmove;
 mod tei;
 
 fn main() {
-    let pos = "x,1,x4/x,1,2,x3/x,1,2,x3/x,1,2,x3/x,1,2,x3/2,1,x4 2 6"
-        .parse::<Position>()
-        .unwrap();
-
-    assert!(pos.has_road(Player::P1));
-    assert!(!pos.has_road(Player::P2));
-
-    println!("nya");
-
     tei::run();
 }
