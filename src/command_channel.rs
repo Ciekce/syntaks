4bd9bd6af2f8b91171debffc14460dc7c2a32058
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-03-19 09:28:15 +0000

    replace ide-generated copyright header

--- src/command_channel.rs
+++ src/command_channel.rs
@@ -1,25 +1,9 @@
-/*
- * syntaks, a TEI Tak engine
- * Copyright (c) 2026 Ciekce
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+// Borrowed from icarus, with permission
+// https://github.com/Sp00ph/icarus/blob/bc1e3b8b84263ff0c14d9e7ef89816f5d01af3f5/src/util/command_channel.rs
+
+//! One-capacity spmc broadcast channel, allowing the UCI thread to send a message
+//! to all search threads. The sending thread blocks until all receiving threads have
+//! handled a sent message.
 
 use std::{
     ptr,
